Benya9669/android_vendor_resurrection

vendor: prebuilt: Add Lawnchair
use Lawnchair 10.0 Alpha 8

config/common.mk
@@ -195,3 +195,14 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/rr/config/partner_gms.mk
+
+# Launchers
+
+ifeq ($(TARGET_LAUNCHER),lawnchair)
+include vendor/rr/prebuilt/app/Lawnchair/lawnchair.mk
+else
+     $(warning TARGET_LAUNCHER is invalid or undefined, building Trebuchet as default launcher)
+
+PRODUCT_PACKAGES += \
+    Launcher3QuickStep
+endif

prebuilt/app/Lawnchair/lawnchair.mk
@@ -1,0 +1,9 @@
+include $(call first-makefiles-under,$(LOCAL_PATH))
+
+PRODUCT_COPY_FILES += \
+    vendor/rr/prebuilt/app/Lawnchair/etc/permissions/privapp-permissions-ch.deletescape.lawnchair.ci.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-ch.deletescape.lawnchair.ci.xml \
+    vendor/rr/prebuilt/app/Lawnchair/etc/sysconfig/lawnchair-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-whitelist.xml
+
+PRODUCT_PACKAGES += \
+    Lawnchair \
+    LawnchairRecentsProvider

Fork-ColtOS/platform_vendor_colt

 * use Lawnchair 10.0 Alpha 8
Signed-off-by: rayrzy <vincentrayananda@gmail.com>
Change-Id: I87d08b7f70478e2fd188e849731d06959bc59050

config/common.mk
@@ -256,3 +256,13 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/colt/overlay
 
 # Enable ccache
 USE_CCACHE := true
+
+# Launchers
+ifeq ($(TARGET_LAUNCHER),lawnchair)
+include vendor/colt/prebuilt/app/Lawnchair/lawnchair.mk
+else
+     $(warning TARGET_LAUNCHER is invalid or undefined, building Launcher3 as default launcher)
+
+PRODUCT_PACKAGES += \
+    Launcher3QuickStep
+endif

prebuilt/app/Lawnchair/lawnchair.mk
@@ -1,0 +1,9 @@
+include $(call first-makefiles-under,$(LOCAL_PATH))
+
+PRODUCT_COPY_FILES += \
+    vendor/colt/prebuilt/app/Lawnchair/etc/permissions/privapp-permissions-ch.deletescape.lawnchair.ci.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-ch.deletescape.lawnchair.ci.xml \
+    vendor/colt/prebuilt/app/Lawnchair/etc/sysconfig/lawnchair-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-whitelist.xml
+
+PRODUCT_PACKAGES += \
+    Lawnchair \
+    LawnchairRecentsProvider